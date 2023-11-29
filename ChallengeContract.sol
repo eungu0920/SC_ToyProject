37e7caea86940f2602223a9d33a25a5b583d77b6
Gray <eungu0920@inu.ac.kr>
2023-11-29 16:28:01 +0900

    Added and modified some codes.

--- ChallengeContract.sol
+++ ChallengeContract.sol
@@ -5,7 +5,7 @@ import "./TimestampConversion.sol";
 
 /* 
     TODO List:
-    1. 참가신청기간 설정 [V]
+    1. 참가신청기간 설정 / 언제 create되던, 다음날 오후9시 신청 마감 [V]
     2. 참가신청기간 끝나면 모인 돈 USDT로 swap 후 staking OR ETH 그대로 높은 이자율 주는 곳에다 staking?
     3. 챌린지를 어떤식으로 인증해야할지 -> 일단 아침 기상 챌린지로 -> 6AM ~ 6:05AM 안에 기상 인증 체크
     4. 챌린지가 끝난 후 상금 분배 방식을 정해야함 -> 리워드 분배 함수
@@ -27,11 +27,10 @@ contract ChallengeContract {
         uint duration;
         bool completed;
 
+        uint numOfWakeUpCheckToWin;
+
         // 마지막 check 때 numOfWinners도 같이 갱신해서 이 변수로 total reward를 나눠주려고함.
         uint numOfWinners;
-
-        // 구조체에 address 배열 말고 highestCheck를 매번 갱신해서
-        uint highestCheck;
 
         // 아침에 일어나는 챌린지라면 아침에 체크하는식으로?
         mapping(address => uint) check;
@@ -56,6 +55,14 @@ contract ChallengeContract {
     event ChallengeCreated(uint challengeId, string challengeName, uint entryAmount, address creator);
     event ChallengeJoined(uint challengeId, address participant);
     event ChallengeCompleted(uint challengeId);
+    event ChallengeClosureScheduled(uint challengeId, uint closureDate);
+    event ChallengeRewardClaimed(uint challengeId, address participant);
+
+    modifier validChallengeIdWithCompleted(uint _challengeId) {
+        require(_challengeId < challenges.length, "Invalid challenge ID");
+        require(!challenges[_challengeId].completed, "Challenge already completed");    
+        _;
+    }
 
     modifier validChallengeId(uint _challengeId) {
         require(_challengeId < challenges.length, "Invalid challenge ID");
@@ -73,54 +80,56 @@ contract ChallengeContract {
         newChallenge.entryAmount = _entryAmount;
         newChallenge.totalAmount += _entryAmount;
         newChallenge.creationTime = block.timestamp;
-        newChallenge.applicationDeadline = block.timestamp + 1 days; // 참가기간은 24시간
+        newChallenge.applicationDeadline = block.timestamp + 1 days; // 참가기간은 다음날 오후 9시 전까지
         newChallenge.duration = _duration;
         newChallenge.completed = false;
+        newChallenge.numOfWakeUpCheckToWin = _duration / 86400; // 예(1일 : 86400초 진행 => winner list에 들기 위한 횟수 : 1번)
         newChallenge.participate[msg.sender] = true;
 
         uint challengeId = challenges.length - 1;
         emit ChallengeCreated(challengeId, _challengeName, _entryAmount, msg.sender);
-
-        // 챌린지가 끝날 때, 신청마감 후 바로 시작
         emit ChallengeClosureScheduled(challengeId, newChallenge.applicationDeadline + _duration);
-    }
-
-    event ChallengeClosureScheduled(uint challengeId, uint closureTime);
+    }    
 
     // 챌린지가 끝나면 자동으로 끝나게 만들라 하는데 나중에 다시 수정해야 할 것 같음...
     function automaticallyCloseChallenge(uint _challengeId) public {
         require(block.timestamp >= challenges[_challengeId].applicationDeadline + challenges[_challengeId].duration, "Challenge duration not over yet");
-        closeChallenge(_challengeId);
+        _closeChallenge(_challengeId);
     }
 
-    // join 할 때, 참가신청기간이 지났는지 확인해야함
-    function joinChallenge(uint _challengeId) public payable validChallengeId(_challengeId) {
-        require(block.timestamp < challenges[_challengeId].applicationDeadline, "The application period has passed.");
-        require(challenges[_challengeId].participate[msg.sender] == false, "You've already joined.");
+    // join 할 때, 참가신청기간이 지났는지 확인해야함, 참가신청기간은 다음날 오후 9시 전까지만
+    function joinChallenge(uint _challengeId) public payable validChallengeIdWithCompleted(_challengeId) {
+        (uint appYear, uint appMonth, uint appDay,,) = challenges[_challengeId].applicationDeadline.timestampToDate();
+        (uint crrYear, uint crrMonth, uint crrDay, uint crrHour,) = block.timestamp.timestampToDate();
+
+        require(crrYear < appYear || crrMonth < appMonth || crrDay < appDay || crrHour < 21, "The application period has expired."); // 오후 21시 전 이면 신청 가능
+        require(!challenges[_challengeId].participate[msg.sender], "You've already joined.");
         require(msg.value == challenges[_challengeId].entryAmount, "Incorrect entry amount");
 
+        emit ChallengeJoined(_challengeId, msg.sender);
         challenges[_challengeId].participate[msg.sender] = true;
         challenges[_challengeId].totalAmount += challenges[_challengeId].entryAmount;
 
-        // -----> 여기 변경해야함 <-----
-        challenges[_challengeId].check[msg.sender] = msg.value;
-
-        emit ChallengeJoined(_challengeId, msg.sender);
     }
 
     // 챌린지 종료, 굳이 creator가 종료 하는 것이아니라 자동으로 되게 할 방법을 생각해봐야함.
-    function closeChallenge(uint _challengeId) public validChallengeId(_challengeId) {
-        require(challenges[_challengeId].creator == msg.sender, "Only the challenge creator can close it");
-        require(!challenges[_challengeId].completed, "Challenge already completed");
+    function _closeChallenge(uint _challengeId) internal validChallengeIdWithCompleted(_challengeId) {
+        require(challenges[_challengeId].creator == msg.sender, "Only the challenge creator can close it");        
+
+        uint closeTime = challenges[_challengeId].applicationDeadline + challenges[_challengeId].duration;
+
+        (uint crrYear, uint crrMonth, uint crrDay, uint crrHour,) = block.timestamp.timestampToDate();        
+        (uint closeYear, uint closeMonth, uint closeDay,,) = closeTime.timestampToDate();
 
         // 챌린지가 안끝났을 때.
-        require(block.timestamp >= challenges[_challengeId].creationTime + challenges[_challengeId].duration, "Challenge duration not over yet");
+        require(crrYear > closeYear || crrMonth > closeMonth || crrDay > closeDay || crrHour >= 21, "Challenge duration not over yet.");
 
         /*
         챌린지가 끝나고 winner를 뽑을 때,
         챌린지가 7일동안 진행된 경우 7일을 모두 체크한 사람이 winners list에 들어감.
         for문 두 번 반복할 필요 없이 한번만 사용하고 챌린지를 모두 참여 완료한 사람만 상금을 분배하는 방식으로 진행.
         즉 하루만 실패해도 상금이 날라감(아니면 리워드 분배방식을 달성 퍼센트별로 설정해야하는데 이거는 지금하기 어려울 것이라고 생각됨)
+        => close 될 때, winners를 뽑는게 아닌 매번 체크해서 
         */
 
         challenges[_challengeId].completed = true;
@@ -149,15 +158,15 @@ contract ChallengeContract {
         // 현재 진행 중인 챌린지 정보를 배열에 저장
         for (uint i = 0; i < challenges.length; i++) {
             if (!challenges[i].completed) {
-                (uint year, uint month, uint day, uint hour,) = (challenges[i].applicationDeadline + challenges[i].duration).timestampToDate();
-                (uint appYear, uint appMonth, uint appDay, uint appHour,) = challenges[i].applicationDeadline.timestampToDate();
+                (uint year, uint month, uint day,,) = (challenges[i].applicationDeadline + challenges[i].duration).timestampToDate();
+                (uint appYear, uint appMonth, uint appDay,,) = challenges[i].applicationDeadline.timestampToDate();
                 ongoingChallenges[index] = OngoingChallenge({
                     id: i,
                     challengeName: challenges[i].challengeName,
                     entryAmount: challenges[i].entryAmount,
                     currentTotalAmount: challenges[i].totalAmount,
-                    appDeadline: [appYear, appMonth, appDay, appHour],
-                    dueDate: [year, month, day, hour]
+                    appDeadline: [appYear, appMonth, appDay, 21],
+                    dueDate: [year, month, day, 21]
                 });
                 index++;
             }
@@ -166,14 +175,10 @@ contract ChallengeContract {
         return ongoingChallenges;
     }
 
-    function getCurrentTime() public view returns (uint, uint, uint, uint, uint) {
-        return block.timestamp.timestampToDate();
-    }
-
     // 아침 6시에서 6시 5분 안에 함수를 실행시켜야함.
     // lastCheck를 통해서 하루에 한번만 체크를 할 수 있음.
-    function wakeUpCheck(uint _challengeId) public validChallengeId(_challengeId) {
-        require(challenges[_challengeId].participate[msg.sender] == true, "You're not a participant of this challenge.");
+    function wakeUpCheck(uint _challengeId) public validChallengeIdWithCompleted(_challengeId) {
+        require(challenges[_challengeId].participate[msg.sender], "You're not a participant of this challenge.");
         (uint lastYear, uint lastMonth, uint lastDay,,) = challenges[_challengeId].lastCheck[msg.sender].timestampToDate();
         (uint currentYear, uint currentMonth, uint currentDay, uint currentHour, uint currentMinute) = block.timestamp.timestampToDate();
 
@@ -182,22 +187,24 @@ contract ChallengeContract {
 
         challenges[_challengeId].check[msg.sender]++;
         challenges[_challengeId].lastCheck[msg.sender] = block.timestamp;
+
+        if(challenges[_challengeId].check[msg.sender] == challenges[_challengeId].numOfWakeUpCheckToWin) {
+            challenges[_challengeId].numOfWinners++;
+        }
     }
 
     // 상금 클레임
     function claimRewards(uint _challengeId) public validChallengeId(_challengeId) {
         require(challenges[_challengeId].completed, "Challenge isn't completed yet.");
         require(!challenges[_challengeId].claimed[msg.sender], "Already claimed");
+        require(challenges[_challengeId].check[msg.sender] == challenges[_challengeId].numOfWakeUpCheckToWin, "You didn't win this challenge.");
 
-        uint numOfWinners = challenges[_challengeId].numOfWinners;
-        uint totalReward = challenges[_challengeId].totalAmount;
-        uint distributeRewards = totalReward / numOfWinners;
+        uint distributeRewards = challenges[_challengeId].totalAmount / challenges[_challengeId].numOfWinners;
 
         challenges[_challengeId].claimed[msg.sender] = true;
         payable(msg.sender).transfer(distributeRewards);
 
-        // event 추가예정
-
+        emit ChallengeRewardClaimed(_challengeId, msg.sender);
     }
 
 }
