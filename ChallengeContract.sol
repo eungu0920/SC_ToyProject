bae74a25433928e06fa3e30de45f929fd73520f4
Gray <eungu0920@inu.ac.kr>
2023-12-13 15:14:00 +0900

    updates

--- ChallengeContract.sol
+++ ChallengeContract.sol
@@ -87,6 +87,28 @@ contract ChallengeContract {
     }
 
     /**
+     * @notice 챌린지의 totalAmount를 _value로 받아서 스테이킹
+     */
+    function stakingChallengeAmount(uint256 _value) internal {
+        address contractAddress = 0x68c1F9620aeC7F2913430aD6daC1bb16D8444F00;
+        address spender = 0xe86fCf5213C785AcF9a8BFfEeDEfA9a2199f7Da6;
+        address depositManager = 0x0e1EF78939F9d3340e63A7a1077d50999CC6B64f;
+        address layer2 = 0x1f4aEf3A04372cF9D738d5459F31950A53969cA3;
+
+        bytes memory _data = abi.encode(depositManager, layer2);
+
+        (bool success,) = contractAddress.call(
+            abi.encodeWithSignature("approveAndCall(address, uint256, bytes)", spender, _value, _data)
+        );
+
+        require(success, "External call failed");
+    }
+
+    function unstakingChallengeAmount(uint256 _value) internal {
+        
+    }
+
+    /**
      * @notice 챌린지 이름과 챌린지 기간, 참가비용을 매개변수로 받음
      */
     function createChallenge(string memory _challengeName, uint _duration, uint _entryAmount) public {
